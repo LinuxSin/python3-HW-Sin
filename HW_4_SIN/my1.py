00b48cff98d69dec53a5c8cd67cf068711717f8d
I Sin <mr-zo99@yandex.ru>
2024-10-15 13:01:01 +0300

    HW

---
+++ HW_4_SIN/my1.py
@@ -0,0 +1,14 @@
+import module_a
+import module_b
+import module_c
+import module_d
+import module_e
+import module_g
+
+
+module_a.Proizved_Matrix()
+module_b.Proizved_Matrix_Vectr()
+module_c.Sled_Matrix()
+module_d.Sklr_Proizved_two_vectros()
+module_e.Gistograms()
+module_g.File_R_W()
